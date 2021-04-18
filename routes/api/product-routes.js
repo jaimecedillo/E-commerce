a62e9521c3367c079251dc2697b34aed0a811fcc
jaime <jaime@MacBook-Pro.local>
2021-04-17 19:40:47 -0600

    completed product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -36,6 +36,22 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
